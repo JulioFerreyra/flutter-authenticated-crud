24632f9ea7019dfc00cbb3bb35feabe715e1ef52
Julio Adrian Ferreyra <jaft210403@outlook.com>
2023-07-20 13:15:31 -0600

    providers creados

---
+++ lib/features/products/presentation/providers/products_repository_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/products/products.dart';
+
+import '../../../auth/presentation/providers/providers.dart';
+
+final productsRepositoryProvider = Provider<ProductsRepository>((ref) {
+  final accessToken = ref.watch(authProvider).user?.token ?? "";
+  final productsRepository =
+      ProductRespositoryImpl(ProductDatasourceImpl(accessToken: accessToken));
+
+  return productsRepository;
+});
